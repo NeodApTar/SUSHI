0f6bea806cff38f6d634574e4381e1097495e083
Алексей <aleynik.alex@yandex.ru>
2025-03-03 18:48:56 +0300

    Create Header.jsx

---
+++ SUSHI/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+//import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header__contents">
+            <h2>Закажи свои любимые суши!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis asperiores molestiae optio nam ab eius totam harum corrupti doloribus hic voluptates, cumque rerum aperiam quaerat ad, voluptate vel obcaecati!</p>
+        <button>Меню</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
